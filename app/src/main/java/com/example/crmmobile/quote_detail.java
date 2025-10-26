d8f0cb74c086f3d470deff51f63e79875a07ecbb
phong571737 <phong571737@gmail.com>
2025-10-26 12:57:46 +0700

    update code

--- app/src/main/java/com/example/crmmobile/quote_detail.java
+++ app/src/main/java/com/example/crmmobile/quote_detail.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class quote_detail extends Fragment {
+    ImageView iv_back;
 
     public quote_detail() {
         // Required empty public constructor
@@ -22,6 +24,17 @@ public class quote_detail extends Fragment {
     }
 
     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        iv_back = view.findViewById(R.id.iv_back);
+
+        //back to quote layout
+        iv_back.setOnClickListener(v -> {
+            requireActivity().finish();
+        });
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
@@ -30,6 +43,7 @@ public class quote_detail extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quote_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_quote_detail, container, false);
+        return view;
     }
 }
