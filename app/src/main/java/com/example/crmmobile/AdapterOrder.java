f599dc57585914915eea51c33814015b0bce6b9f
chitken-dev <lethianhxuan1014@gmail.com>
2025-11-13 11:58:04 +0700

    refactor opportunity bottom sheet both layout and java logic

--- app/src/main/java/com/example/crmmobile/AdapterOrder.java
+++ app/src/main/java/com/example/crmmobile/AdapterOrder.java
@@ -107,8 +107,8 @@ public class AdapterOrder extends RecyclerView.Adapter<AdapterOrder.OrderViewHol
     // 🔹 Hàm tạo 1 item_action
     private void addActionItem(LinearLayout parent, int iconRes, String text, Runnable onClick) {
         View itemView = LayoutInflater.from(context).inflate(R.layout.item_action, parent, false);
-        ImageView icon = itemView.findViewById(R.id.actionIcon);
-        TextView label = itemView.findViewById(R.id.actionText);
+        ImageView icon = itemView.findViewById(R.id.img_action_icon);
+        TextView label = itemView.findViewById(R.id.tv_action);
         icon.setImageResource(iconRes);
         label.setText(text);
 
